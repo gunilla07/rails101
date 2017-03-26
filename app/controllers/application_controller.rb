e388144c73cb7c332ad5d0fce0db143ca11cc323
gunilla <gunilla1148@gmail.com>
2017-03-26 16:08:38 +0800

    add post button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-class GroupsController < ApplicationController
-
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-
-
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -13,10 +12,25 @@ class GroupsController < ApplicationController
  def new
    @group = Group.new
  end
-def edit
+ def edit
+
+ end
+
+ def create
+  @group = Group.new(group_params)
+  @group.user = current_user
+
+  if @group.save
+    redirect_to groups_path
+  else
+    render :new
   end
+end
+
+
 
   def update
+
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
@@ -25,16 +39,15 @@ def edit
   end
 
   def destroy
+    
     @group.destroy
     redirect_to groups_path, alert: "Group deleted"
   end
 
 
-    @group.destroy
-    redirect_to groups_path, alert: "Group deleted"
-  end
 
  private
+
  def find_group_and_check_permission
     @group = Group.find(params[:id])
 
@@ -42,12 +55,6 @@ def edit
       redirect_to root_path, alert: "You have no permission."
     end
   end
-
-  def group_params
-    params.require(:group).permit(:title, :description)
-  end
-
-end
 
 
  def group_params
